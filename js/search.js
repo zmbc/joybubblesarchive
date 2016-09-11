df9583d52e8c5deae0bf8d5f597c6c998ec24ebf
Zeb Burke-Conte <zmbc@uw.edu>
2016-09-11 13:07:47 -0700

    publish: rm unused plugin
    generated from commit eb7a408c94bb2e7b9aa649a0d3ec97ff721f6a12

---
+++ js/search.js
@@ -0,0 +1,64 @@
+(function() {
+  function displaySearchResults(results, store) {
+    var searchResults = document.getElementById('search-results');
+
+    if (results.length) { // Are there any results?
+      var appendString = '';
+
+      for (var i = 0; i < results.length; i++) {  // Iterate over the results
+        var item = store[results[i].ref];
+        appendString += '<div class="box"><a href="' + item.url.replace('.html', '') + '"><h3 style="color:inherit">' + item.name + '</h3></a>';
+        appendString += '<p style="margin-bottom:0">' + item.date + '</p>';
+        if (item.content.length > 0) {
+          appendString += '<p>' + item.content.substring(0, 150) + '...</p></li>';
+        } else {
+          appendString += '<p>No description.</p></div>';
+        }
+      }
+
+      searchResults.innerHTML = appendString;
+    } else {
+      searchResults.innerHTML = '<li>No results found</li>';
+    }
+  }
+
+  function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split('=');
+
+      if (pair[0] === variable) {
+        return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
+      }
+    }
+  }
+
+  var searchTerm = getQueryVariable('query');
+
+  if (searchTerm) {
+    document.getElementById('search-box').setAttribute("value", searchTerm);
+
+    // Initalize lunr with the fields it will be searching on. I've given title
+    // a boost of 10 to indicate matches on this field are more important.
+    var idx = lunr(function () {
+      this.field('id');
+      this.field('name', { boost: 10 });
+      this.field('tags');
+      this.field('content');
+    });
+
+    for (var key in window.store) { // Add the data to lunr
+      idx.add({
+        'id': key,
+        'name': window.store[key].name,
+        'tags': window.store[key].tags,
+        'content': window.store[key].content
+      });
+
+      var results = idx.search(searchTerm); // Get lunr to perform a search
+      displaySearchResults(results, window.store);
+    }
+  }
+})();
